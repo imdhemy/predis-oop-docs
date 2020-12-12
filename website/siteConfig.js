a037a0a01d2c5bd6b226a3fd328c9bbc1929efbe
imdhemy <imdhemy@gmail.com>
2020-12-12 12:05:31 +0200

    Update site configs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://imdhemy.github.io', // Your website URL
-  baseUrl: '/predis-oop-docs', // Base URL for your project */
+  baseUrl: '/predis-oop-docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'predis-oop-docs',
   organizationName: 'imdhemy',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
